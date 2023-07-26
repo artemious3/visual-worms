90b7e8b95084c0abe842382f21ecd2448265637c
artemious3 <artempodgaisky@gmail.com>
2023-07-26 21:56:22 +0300

    Add files via upload

--- src/SI/Common.h
+++ src/SI/Common.h
@@ -22,19 +22,19 @@ typedef std::set<std::pair<float, float>> positionsSet;
 
 //_______________________PARAMETERS__________________________
 
-#define _ANGLE 60.0f //angle between side detectors and y-axis
-#define _LENGTH 14.0f //length of detector
-#define _dANGLE 20.0f //angle change when detector explores substance
+#define _ANGLE 40.0f //angle between side detectors and vertical detector
+#define _LENGTH 20.0f //length of detector
+#define _dANGLE 20.0f //chagne of agent's direction after side detectors explore more substance
 
 #define _DIFUSSION 0.003f; //how fast the substance disappear
 #define _cANGLE 0.2f //every update() call
-                    //the agent rotates to _cANGLE
+                    //the agent changes its direction by _cANGLE
 
-#define _SIZE 3.0f //size of agent (px)
+#define _SIZE 2.0f //size of agent (px)
 #define _WINSIZE 1500 //size of window
 #define _AMOUNT 1400 //amount of agents
-#define _SPEED 5.0f //every update() call agent moves <_SPEED> px
-#define _BORDER 100 //distance between window bound and bound of agents area
+#define _SPEED 2.0f //every update() call the agent moves by <_SPEED> px
+#define _BORDER 100 //distance between window bound and bound of agents' area
 
 #define _COLOR_DIFUSSION 0.0006f //how fast the color of substance changes
 
